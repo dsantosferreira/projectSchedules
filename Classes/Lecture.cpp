cd0784f65209294a1cc26f535a5d4145c0559ae3
DANIEL <danielcb7@hotmail.com>
2022-10-24 18:55:00 +0100

    Implementing methods of Lecture Class

--- Classes/Lecture.cpp
+++ Classes/Lecture.cpp
@@ -1,5 +1,34 @@
 //
 // Created by franciscocardoso on 18-10-2022.
 //
-
+#include <iostream>
+#include <string>
 #include "Lecture.h"
+
+Lecture::Lecture(std::string weekDay_, float start_, int duration, std::string type_) {
+    this->weekDay = weekDay_;
+    this->lectureTime = pair<float, float>(start_,start_ + duration);
+    this->type = type_;
+}
+
+string Lecture::getType() const {
+    return this->type;
+}
+
+pair<float, float> Lecture::getLectureTime() const {
+    return this->lectureTime;
+}
+
+int Lecture::getDuration() const {
+    return this->lectureTime.second - this->lectureTime.first;
+}
+
+/*
+int Lecture::endTime() const{
+    return this->lectureTime.second;
+ }
+*/
+
+void Lecture::print() const {
+    cout << this->weekDay << " " << this->lectureTime.first << " " << this->lectureTime.first + this->lectureTime.second << " " << this->type;
+}
